74b832669c7772549fd178f45f5f9065c18c4fa5
mosehmaguah <mosehmaguah@gmail.com>
2024-03-01 03:04:59 +0300

    fixed some issues

--- stopwatch-app/src/Stopwatch.jsx
+++ stopwatch-app/src/Stopwatch.jsx
@@ -1,67 +1,68 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-function Stopwatch(){
+function Stopwatch() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const intervalIdRef = useRef(null);
+  const startTimeRef = useRef(0);
 
-    const [isRunning, setIsRunning] = useState(false);
-    const [elapsedTime, setElapsedTime] =useState(0);
-    const intervalIdRef = useRef(null);
-    const startTimeRef = useRef(0);
+  useEffect(() => {
+      if(isRunning) {
+        intervalIdRef.current = setInterval(() => {
+          setElapsedTime(Date.now() - startTimeRef.current);
+        }, 10);
+      }
 
-    useEffect(() => {
-        if(isRunning){
-            intervalIdRef.current = setInterval(() => {
-                setElapsedTime(Date.now() - startTimeRef.current);   
-            }, 10);
-        }
+      return () => {
+        clearInterval(intervalIdRef.current);
+      };
+    },[isRunning]);
 
-        return () => {
-            clearInterval(intervalIdRef.current);
-        }
-    }, {isRunning});
+  function start() {
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - elapsedTime;
+    console.log(startTimeRef.curren);
+  }
 
-    function start(){
-        setIsRunning(true);
-        startTimeRef.current = Date.now() - elapsedTime;
-    }
+  function stop() {
+    setIsRunning(false);
+  }
 
-    function stop(){
-        setIsRunning(false);
-    }
+  function reset() {
+    setElapsedTime(0);
+    setIsRunning(false);
+  }
 
-    function reset(){
-        setElapsedTime(0);
-        setIsRunning(false);
-    }
+  function formatTime() {
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    let seconds = Math.floor((elapsedTime / 1000) % 60);
+    let millliseconds = Math.floor((elapsedTime % 1000) / 10);
 
-    function formatTime(){
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    millliseconds = String(millliseconds).padStart(2, "0");
 
-        let hours =Math.floor(elapsedTime / (1000 * 60 * 60));
-        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
-        let seconds = Math.floor(elapsedTime / (1000) % 60);
-        let millliseconds = Math.floor((elapsedTime % 1000) / 10);
+    return `${minutes}:${seconds}:${millliseconds}`;
+  }
 
-        hours = String(hours).padStart(2, "0");
-        minutes = String(minutes).padStart(2, "0");
-        seconds = String(seconds).padStart(2, "0");
-        millliseconds = String(millliseconds).padStart(2, "0");
-
-        return `${minutes}:${seconds}:${millliseconds}`;
-    }
-
-
-    return (
-        <div className='stopwatch'>
-            <div className='display'>
-                {formatTime()}
-            </div>
-            <div className='controls'>
-                <button onClick={start} className='start-button' >Start</button>
-                <button onClick={stop} className='stop-button' >Stop</button>
-                <button onClick={reset} className='reset-button' >Reset</button>
-            </div>
-            
-        </div>
-    );
+  return (
+    <div className="stopwatch">
+      <div className="display">{formatTime()}</div>
+      <div className="controls">
+        <button onClick={start} className="start-button">
+          Start
+        </button>
+        <button onClick={stop} className="stop-button">
+          Stop
+        </button>
+        <button onClick={reset} className="reset-button">
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Stopwatch;
